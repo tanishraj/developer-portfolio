4a4795e4f3c8c351129964cad4429fdee1931583
Tanishraj <114914575+tanishrajkumar@users.noreply.github.com>
2023-08-30 19:59:07 +0400

    feat: hexagon with animation

--- src/lib/components/base/hexagon.tsx
+++ src/lib/components/base/hexagon.tsx
@@ -1,42 +1,78 @@
-import { Box, BoxProps, ResponsiveValue } from '@chakra-ui/react';
+import { Box, BoxProps, ResponsiveValue, keyframes } from '@chakra-ui/react';
 
 type IconStyleType = {
   iconStyle?: BoxProps;
   borderStyle?: BoxProps;
+  animationStyle?: BoxProps;
 };
 
 interface HexagonProps extends BoxProps {
   size?: ResponsiveValue<string>;
   outlineSize?: string;
+  isAnimate?: boolean;
   hexagonStyles?: IconStyleType;
 }
 
 export const Hexagon = (props: HexagonProps) => {
-  const { size = 'md', outlineSize = 0, hexagonStyles, ...restProps } = props;
+  const {
+    size = 'md',
+    outlineSize = 0,
+    hexagonStyles,
+    isAnimate = false,
+    ...restProps
+  } = props ?? {};
+
+  const pulseEffect = keyframes`
+  from {
+    transform: scale(1);
+    opacity: 1;
+  }
+  to {
+    transform: scale(2);
+    opacity: 0; 
+  }
+`;
 
   return (
     <Box
       position='relative'
       height={size}
       width={size}
+      cursor='pointer'
       {...restProps}
     >
       <Box
-        height={size}
-        width={size}
-        position={outlineSize ? 'absolute' : 'relative'}
+        position='absolute'
+        height={`calc(100% - ${outlineSize} - ${outlineSize})`}
+        width={`calc(100% - ${outlineSize} - ${outlineSize})`}
+        top={`${outlineSize}`}
+        left={`${outlineSize}`}
+        zIndex={1}
         clipPath='polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)'
         {...hexagonStyles?.iconStyle}
       />
       {outlineSize && (
         <Box
-          position='absolute'
-          height={`calc(100% - ${outlineSize} - ${outlineSize})`}
-          width={`calc(100% - ${outlineSize} - ${outlineSize})`}
-          top={`${outlineSize}`}
-          left={`${outlineSize}`}
+          height={size}
+          width={size}
+          position={outlineSize ? 'absolute' : 'relative'}
+          zIndex={0}
           clipPath='polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)'
           {...hexagonStyles?.borderStyle}
+        />
+      )}
+      {isAnimate && (
+        <Box
+          position='absolute'
+          height={size}
+          width={size}
+          top={0}
+          left={0}
+          zIndex={-1}
+          background='white'
+          animation={`${pulseEffect} 1.5s ease infinite`}
+          clipPath='polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)'
+          {...hexagonStyles?.animationStyle}
         />
       )}
     </Box>
