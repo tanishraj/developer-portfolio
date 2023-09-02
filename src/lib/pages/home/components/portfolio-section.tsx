7b9f150e11ddd6594d8e78bf8c4a212c1a1481a9
Saif Sheikh <105776315+saif-bsf@users.noreply.github.com>
2023-09-02 15:31:39 +0400

    feat: portfolio (#21)

---
+++ src/lib/pages/home/components/portfolio-section.tsx
@@ -0,0 +1,73 @@
+import { Container, Flex, Grid, Heading } from '@chakra-ui/react';
+import { PortfolioItem } from '../../../components/base/portfolio-item';
+
+export const PortfolioSection = () => {
+  return (
+    <Container>
+      <Flex
+        flex='1'
+        height='full'
+        flexFlow='column'
+        justifyContent='center'
+        minHeight='100vh'
+        p={{ base: '2xs', md: 'sm', lg: 'lg', xl: 'xl' }}
+      >
+        <Heading
+          mb={{ base: '5xl', md: '2xl' }}
+          size={{ base: 'md-desktop', md: 'lg-desktop', lg: 'xl-desktop' }}
+        >
+          Portfolio
+        </Heading>
+        <Grid
+          h='400px'
+          templateRows={{ base: 'repeat(4, 1fr)', md: 'repeat(2, 1fr)' }}
+          templateColumns={{ base: 'repeat(3, 1fr)', md: 'repeat(6, 1fr)' }}
+          gap={2}
+        >
+          <PortfolioItem
+            cols={1}
+            rows={1}
+            imgSrc='./public/assets/images/project-1.jpg'
+            text='Responsive Web Application'
+          />
+          <PortfolioItem
+            cols={1}
+            rows={1}
+            imgSrc='./public/assets/images/project-2.jpg'
+            text='Responsive Mobile Application'
+          />
+          <PortfolioItem
+            cols={1}
+            rows={2}
+            imgSrc='./public/assets/images/project-6.jpg'
+            text='Responsive IOS Application'
+          />
+          <PortfolioItem
+            cols={3}
+            rows={1}
+            imgSrc='./public/assets/images/project-4.jpg'
+            text='Responsive Web Application'
+          />
+          <PortfolioItem
+            cols={2}
+            rows={1}
+            imgSrc='./public/assets/images/project-5.jpg'
+            text='Responsive Web3 Application'
+          />
+          <PortfolioItem
+            cols={1}
+            rows={1}
+            imgSrc='./public/assets/images/project-3.jpg'
+            text='Responsive Blockchain Application'
+          />
+          <PortfolioItem
+            cols={{ base: 3, md: 2 }}
+            rows={1}
+            imgSrc='./public/assets/images/project-7.jpg'
+            text='Responsive Web Application'
+          />
+        </Grid>
+      </Flex>
+    </Container>
+  );
+};
--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -2,6 +2,7 @@ import { AboutSection } from './components/about-section';
 import { ExpertiseSection } from './components/expertise-section';
 import { HeroSection } from './components/hero-section';
 import { JourneySection } from './components/journey-section';
+import { PortfolioSection } from './components/portfolio-section';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <AboutSection />
       <ExpertiseSection />
       <JourneySection />
+      <PortfolioSection />
     </>
   );
 };
