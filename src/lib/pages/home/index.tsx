4ddf9aec4279298d0a7a0d2cc9a543552722c728
Tanishraj <114914575+tanishrajkumar@users.noreply.github.com>
2023-09-06 23:51:06 +0400

    feat: adding json data from a local file

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -5,8 +5,10 @@ import { HeroSection } from './components/hero-section';
 import { JourneySection } from './components/journey-section';
 import { PortfolioSection } from './components/portfolio-section';
 import { SkillsSection } from './components/skills-section';
+import { portfolioData } from '../../../data/portfolio-data';
 
 const Home = () => {
+  console.log('API DATA', portfolioData);
   return (
     <>
       <HeroSection />
