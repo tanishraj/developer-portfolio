7394a8e97e09be205f7e12386219a6958b6cccdd
Tanishraj <114914575+tanishrajkumar@users.noreply.github.com>
2023-09-07 00:26:53 +0400

    feat: adding json data from a local file (#37)

---
+++ src/data/portfolio-data.ts
@@ -0,0 +1,284 @@
+export const portfolioData = {
+  createdAt: '2023-08-19T11:25:03Z',
+  _id: 'e1c08282-d0ed-4b85-bb86-9594ec7cadd7',
+  _rev: 'jxxy9bfklLqTPN1tcPqI9k',
+  _type: 'pageBuilder',
+  _updatedAt: '2023-08-20T02:27:40Z',
+  components: [
+    {
+      _key: '839252162d49',
+      _type: 'heroSection',
+      subtitle: 'Frontend Engineer',
+      title: 'Tanish Raj',
+    },
+    {
+      _key: 'bab5e62ddfd0',
+      _type: 'aboutSection',
+      content: [
+        {
+          _key: '71d77ac6d323',
+          _type: 'block',
+          children: [
+            {
+              _key: '55a3f7e52e6b0',
+              _type: 'span',
+              marks: [],
+              text: "\"Hello, I'm Tanish. I discovered my passion for coding during my time at , where I obtained my degree in Master of Computer Application. Since then, I've spent the past 6 years working in various roles as a Frontend Engineer, always pushing myself to learn and adapt to the ever-changing landscape of web technologies.",
+            },
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+        {
+          _key: '58298b01bb31',
+          _type: 'block',
+          children: [
+            {
+              _key: '114edc9670d80',
+              _type: 'span',
+              marks: [],
+              text: 'Specialising in React and Vue.js, I pride myself on my attention to detail, problem-solving capabilities, and a keen eye for design. I believe that the success of a project lies not only in its functionality but also in its ability to deliver an exceptional user experience.',
+            },
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+        {
+          _key: 'db2e3244407c',
+          _type: 'block',
+          children: [
+            {
+              _key: 'c4de5f80423e0',
+              _type: 'span',
+              marks: [],
+              text: "When I'm not coding, you'll find me playing some indoor and outdoor games.",
+            },
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+      ],
+      photo: {
+        _type: 'image',
+        asset: {
+          _ref: 'image-2b297ef0e6f6ac78db1b2ed47af57a73a2214ccf-607x910-jpg',
+          _type: 'reference',
+        },
+      },
+      socialMediaList: [
+        {
+          _key: '67ea5a913bd3',
+          _type: 'socialMedia',
+          socialMediaIcon: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-50c678f300dfc482944f84e7863736089fe9f029-2048x2048-png',
+              _type: 'reference',
+            },
+          },
+          socialMediaLink: 'https://www.linkedin.com/in/itsmetanishraj/',
+        },
+      ],
+      title: 'About me',
+    },
+    {
+      _key: '67f437566c53',
+      _type: 'expertiseSection',
+      expertiseList: [
+        {
+          _key: '7860044c957c',
+          _type: 'yourExpertise',
+          expertiseContent:
+            'I excel at building websites that offer an optimal user experience across all devices and creensizes.',
+          expertiseTitle: 'Adaptive  Design',
+        },
+        {
+          _key: '3ae23e2e2417',
+          _type: 'yourExpertise',
+          expertiseContent:
+            'I craft dynamic, user-centric web applications that effectively cater to diverse business eedsusing React & Vue.',
+          expertiseTitle: 'JavaScript  Framework',
+        },
+        {
+          _key: '59e98d786a20',
+          _type: 'yourExpertise',
+          expertiseContent:
+            'I ensure swift, seamless web experiences through strategies like lazy loading, caching, and odeminification.',
+          expertiseTitle: 'Performance Optimization',
+        },
+      ],
+      title: 'Expertise',
+    },
+    {
+      _key: 'b5411fe981bc',
+      _type: 'experienceSection',
+      title: 'Experience',
+      totalExperience: [
+        {
+          _key: 'bdba2e7bec7f',
+          _type: 'yourExperience',
+          companyName: 'Thomson Reuters',
+          yearOfJoining: 2017,
+          yearOfResignation: 2018,
+        },
+        {
+          _key: '02ebdc8e277f',
+          _type: 'yourExperience',
+          companyName: 'CGI',
+          yearOfJoining: 2017,
+          yearOfResignation: 2018,
+        },
+        {
+          _key: '81ada6f30fc1',
+          _type: 'yourExperience',
+          companyName: 'Rakuten',
+          yearOfJoining: 2017,
+          yearOfResignation: 2018,
+        },
+        {
+          _key: 'a1e4a686b1e6',
+          _type: 'yourExperience',
+          companyName: 'Wells Fargo',
+          yearOfJoining: 2017,
+          yearOfResignation: 2018,
+        },
+        {
+          _key: '19e62696d75b',
+          _type: 'yourExperience',
+          companyName: 'Banque Saudi Fransi',
+          yearOfJoining: 2017,
+          yearOfResignation: 2018,
+        },
+      ],
+    },
+    {
+      _key: '703918d89e0c',
+      _type: 'workSection',
+      mywork: [
+        {
+          _key: '8ccbf0737833',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-9b0838a7d0b7cbbe9e0fdcb1a309687a20f5e338-788x525-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '400e8667b80a',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-c8aee9b6cc48181f4afa9f288ae2bdc3b0f498d5-1200x800-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '6c8f9b1896ed',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-165f2d03eaf95f4ad5249c342016bd18249c92b8-1200x800-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '295c7d729191',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-f7581ef713a732cca6d7aed2c16a9a8e5292fb5c-1800x1200-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '37e0e4728ddd',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-045ce2c597f03dcf27b8272de61e2178bd1d26a9-1200x800-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '0940e3b2cf84',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-c35e4180fab32741708269046a70e565e9d45b62-896x1311-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+        {
+          _key: '07e04e2a7a3d',
+          _type: 'workDetails',
+          workLiveUrl: 'https://www.google.com',
+          workSample: {
+            _type: 'image',
+            asset: {
+              _ref: 'image-ec24e065185eef0fa8fec56b5c291ba24f39b522-1200x800-jpg',
+              _type: 'reference',
+            },
+          },
+        },
+      ],
+      workTitle: 'My Work',
+    },
+    {
+      _key: '92c5aba00f3e',
+      _type: 'contactSection',
+      contactForm: [
+        {
+          _key: 'c246b502a037',
+          _type: 'formControls',
+          controlLable: 'First Name',
+          controlType: 'text',
+        },
+        {
+          _key: '63f85fce6620',
+          _type: 'formControls',
+          controlLable: 'Last Name',
+          controlType: 'text',
+        },
+        {
+          _key: 'a135a11939e4',
+          _type: 'formControls',
+          controlLable: 'Email',
+          controlType: 'email',
+        },
+        {
+          _key: '0704b0a2e778',
+          _type: 'formControls',
+          controlLable: 'Message',
+          controlType: 'textarea',
+        },
+        {
+          _key: '3d39991dcb12',
+          _type: 'formControls',
+          controlLable: 'Send',
+          controlType: 'button',
+        },
+      ],
+      title: 'Contact',
+    },
+  ],
+  pageTitle: 'Home Page',
+};
